45d9195a6b5594a06eb2b19a8667bbc5cd534f1c
Assa <assachana@gmail.com>
2014-11-21 16:11:11 +0000

    Symmetry with the randomly drawn coloured lines.

--- src/BioMorph.java
+++ src/BioMorph.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.Random;
@@ -12,34 +13,37 @@ public class BioMorph extends JPanel {
 	public void doDrawing(Graphics g, int a, int b, int c, int d) {
 
 		Graphics2D g2d = (Graphics2D) g;
-
+		
+		g2d.setColor(generateColour());
 		g2d.drawLine(a, b, c, d);
-
-		a *= -1;
-		b *= -1;
-		c *= -1;
-		d *= -1;
-
-		g2d.drawLine(a, b, c, d);
-
+		g2d.setColor(generateColour());
 	} 
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		for(int l=0; l<10; l++){
+		for(int l=0; l<5; l++){
 			
 			int h = generateInt();
 			int i = generateInt();
 			int j = generateInt();
 			int k = generateInt();
-
+			
 			doDrawing(g,h,i,j,k);
+			doDrawing(g,k,j,i,h);
 		}
 	}    
 
 	public int generateInt(){
 		int i = rand.nextInt((300 - 100) + 1) + 100;
 		return i;
+	}
+	
+	public Color generateColour(){
+		float r = rand.nextFloat();
+		float g = rand.nextFloat();
+		float b = rand.nextFloat();
+		Color rand = new Color(r,g,b);
+		return rand;
 	}
 }
