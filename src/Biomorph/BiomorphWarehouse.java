1ec7032bf40b4dc9101f06616e0cd3684e91c899
Matty <Matty@HomePC>
2014-12-13 16:04:14 +0000

    Changed methods for add and remove in BiomorphWarehouse class

--- src/Biomorph/BiomorphWarehouse.java
+++ src/Biomorph/BiomorphWarehouse.java
@@ -1,4 +1,5 @@
 package Biomorph;
+
 import java.util.ArrayList;
 
 
@@ -14,7 +15,11 @@ public class BiomorphWarehouse {
 		
 	}
 	
-	public void storeTempBiomorph(Biomorph b){
+	/**
+	 * @param b
+	 * @return a true or false value depending if the 
+	 */
+	private Boolean storeTempBiomorph(Biomorph b){
 		Boolean stored = false;
 		while(!stored){
 			for(int i = 0; i < 7; i++){
@@ -22,57 +27,85 @@ public class BiomorphWarehouse {
 					tempBiomorphs.add(b);
 					stored = true;
 				}else if(i == 7){
-					System.out.println("Only 8 biomorph's can be temp stored at a time");
-				}
-			}
-		}
-		//If you want to use an array
-		/*Boolean stored = false;
-		while(!stored){
-			for(int i = 0; i < tempBiomorphs.length; i++){
-				if(tempBiomorphs[i] == null){
-					tempBiomorphs[i] = b;
-					stored = true;
-				}else if(i == tempBiomorphs.length){
-					System.out.println("Only 8 biomorph's can be temp stored at a time");
-				}
-			}
-		}*/
-	}
-	
-	public Boolean storeParentBiomorph(Biomorph b){
-		/*Boolean stored = false;
-		while(!stored){
-			for(int i = 0; i < parentBiomorphs.length; i++){
-				if(tempBiomorphs[i] == null){
-					tempBiomorphs[i] = b;
-					stored = true;
-				}else if(i == parentBiomorphs.length){
-					System.out.println("Only 2 parent biomorph's can be used in the evolution processs");
+					System.err.println("Only 8 Biomorph's can be temp stored at a time");
 					return false;
 				}
 			}
-		}*/
+			
+		}
 		return true;
+							//If you want to use an array
+							/*Boolean stored = false;
+							while(!stored){
+								for(int i = 0; i < tempBiomorphs.length; i++){
+									if(tempBiomorphs[i] == null){
+										tempBiomorphs[i] = b;
+										stored = true;
+									}else if(i == tempBiomorphs.length){
+										System.out.println("Only 8 Biomorph's can be temp stored at a time");
+									}
+								}
+							}*/
+	}
+	
+
+	private Boolean storeParentBiomorph(Biomorph b){
+		Boolean stored = false;
+		while(!stored){
+			for(int i = 0; i < 1; i++){
+				if(parentBiomorphs.get(i) == null){
+					parentBiomorphs.add(b);
+					stored = true;
+				}else if(i == 1){
+					System.err.println("Only 8 Biomorph's can be temp stored at a time");
+					return false;
+				}
+			}
+		}
+		return true;
+								//if you wanted to use an array
+								/*Boolean stored = false;
+								while(!stored){
+									for(int i = 0; i < parentBiomorphs.length; i++){
+										if(tempBiomorphs[i] == null){
+											tempBiomorphs[i] = b;
+											stored = true;
+										}else if(i == parentBiomorphs.length){
+											System.out.println("Only 2 parent Biomorph's can be used in the evolution processs");
+											return false;
+										}
+									}
+								}*/
+	}
+	
+	
+	public void addTempBiomorph(Biomorph b){
+		storeTempBiomorph(b);
+	}
+	
+	public void addParentBiomorph(Biomorph b){
+		storeTempBiomorph(b);
 	}
 	
 	public void removeTempBiomorph(int i){
-		tempBiomorphs[i] = null;
+		tempBiomorphs.remove(i);
 	}
 	
 	public void removeParentBiomorph(int i){
-		parentBiomorphs[i] = null;
+		parentBiomorphs.remove(i);
 	}
 	
 	public void addTempToParent(int tempNum){
-		if(storeParentBiomorph(tempBiomorphs[tempNum]) == true){
-			tempBiomorphs[tempNum] = null;
+		if(storeParentBiomorph(tempBiomorphs.get(tempNum)) == true){
+			tempBiomorphs.remove(tempNum);
 		}
 		
 	}
 	
 	public void addParentToTemp(int tempNum, int parentNum){
-
+		if(storeTempBiomorph(parentBiomorphs.get(parentNum)) == true){
+			parentBiomorphs.remove(parentNum);
+		}
 	}
 
 }
