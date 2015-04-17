df32b5ec65f93b75b8444709cec5c505f70c0ae7
Satpal Singh <singhs23@aston.ac.uk>
2015-04-17 13:49:32 +0100

    Added for loop for displaying lines which is useful for having more lines to display

--- src/view/Renderer.java
+++ src/view/Renderer.java
@@ -1,4 +1,5 @@
 package view;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -29,32 +30,54 @@ public class Renderer extends JPanel {
 	 * @param d - used to draw lines
 	 * @param color - used for color 
 	 */
-	public void displayBiomorph(Graphics g, int a, int b, int c, int d, Color color) {
+	public void displayBiomorph(Graphics g, int x1, int y1, int x2, int y2, Color color) {
 
 		g2d = (Graphics2D) g;
+		//g2d.rotate(Math.toRadians(20));
 		g2d.setColor(color);
-		g2d.drawLine(a, b, c, d);
+			//Can be used for the thickness of the lines
+		 //g2d.setStroke(new BasicStroke(2));
+		g2d.drawLine(x1, y1, x2, y2);
 		
 		
 	}
-
+	
+	
+	private void drawLines(Graphics g, int genes[]){
+		// Color for the lines
+		Color color = new Color(genes[12]);
+		//starting lines
+				displayBiomorph(g, genes[0],genes[1],genes[2],genes[3],color);
+				displayBiomorph(g, genes[2],genes[1],genes[0],genes[3],color);
+				
+				displayBiomorph(g, genes[2],genes[3],genes[4],genes[5],color);
+				int x1 =genes[4]-genes[2] ;
+				int y1 =  genes[5] - genes[3];
+				int x2 = genes[0] -x1;
+				int y2 = genes[3] +y1;
+				displayBiomorph(g, genes[0],genes[3],x2,y2,color);
+				
+				for(int i = 4; i < 10; i+=2){
+					displayBiomorph(g, genes[i],genes[i+1],genes[i+2],genes[i+3],color);
+					x1 =genes[i+2]-genes[i];
+					y1 =  genes[i+3] - genes[i+1];
+					int px2 = x2;
+					int py2 = y2;
+					x2 = x2 -x1;
+					y2 = y2 +y1;
+					displayBiomorph(g, px2,py2,x2,y2,color);
+						
+					}
+		
+	}
+	
 	/* (non-Javadoc)
 	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
 	 */
 	@Override
 	public void paintComponent(Graphics g) {
-		Color color = new Color(genes[12]);
-		//display lines by passing genes value
-		displayBiomorph(g, genes[0],genes[1],genes[2],genes[3],color);
-		displayBiomorph(g, genes[3],genes[2],genes[1],genes[0],color);
-		displayBiomorph(g, genes[2],genes[3],genes[4],genes[5],color);
-		displayBiomorph(g, genes[5],genes[4],genes[3],genes[2],color);
-		displayBiomorph(g, genes[4],genes[5],genes[6],genes[7],color);
-		displayBiomorph(g, genes[7],genes[6],genes[5],genes[4],color);
-		displayBiomorph(g, genes[6],genes[7],genes[8],genes[9],color);
-		displayBiomorph(g, genes[9],genes[8],genes[7],genes[6],color);
-		displayBiomorph(g, genes[8],genes[9],genes[10],genes[11],color);
-		displayBiomorph(g, genes[11],genes[10],genes[9],genes[8],color);
+		
+		drawLines(g, genes);
 
 	}    
 
